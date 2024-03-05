779f0892eea5eea49d0ab9d3323857f492e366d9
Blaise Thompson <blaise@untzag.com>
2024-03-05 16:57:34 -0600

    firmware update

--- firmware/main.py
+++ firmware/main.py
@@ -19,7 +19,6 @@ from homie.property import HomieNodeProperty
 
 from uasyncio import get_event_loop, sleep_ms
 
-
 from max9651 import MAX9651
 
 
@@ -87,16 +86,19 @@ class VoltMeter(HomieNode):
             default="",
         )
         self.add_property(self.ip)
-        self.led = Pin(0, Pin.OUT)
         self.online_led = Pin(15, Pin.OUT)
         self.online_led.off()
         self.last_online = time.time()
         self.start = time.time()
         print("start time", self.start)
+        self.currently_displayed_channel = 0
         self.measured_voltages = {}
         loop = get_event_loop()
         loop.create_task(self.measure_voltages())
         loop.create_task(self.update_data())
+        # interrupt on button press
+        pir = Pin(2, Pin.IN)
+        pir.irq(trigger=Pin.IRQ_RISING, handler=self.on_button_press)
 
     async def update_data(self):
         # wait until connected
@@ -114,12 +116,10 @@ class VoltMeter(HomieNode):
                 print(1)
                 self.online_led.on()
                 print(2)
-                self.led.value(0)  # illuminate onboard LED
                 for index in range(4):
                     self.voltage_properties[index].data = str(self.measured_voltages[index])
                 self.uptime.data = self.get_uptime()
                 self.ip.data = network.WLAN().ifconfig()[0]
-                self.led.value(1)  # onboard LED off
                 print("final")
                 await sleep_ms(15_000)
             while not self.device.mqtt.isconnected():
@@ -127,16 +127,19 @@ class VoltMeter(HomieNode):
                 if time.time() - self.last_online > 300:   # 5 minutes
                     machine.reset()
                 self.online_led.off()
-                self.led.value(0)  # illuminate onboard LED
-                await sleep_ms(100)
-                self.led.value(1)  # onboard LED off
                 await sleep_ms(1000)
             machine.reset()  # if lost connection, restart
 
     async def measure_voltages(self):
         while True:
             for index in range(4):
-                self.i2c.writeto(104, (0b1001_1000).to_bytes(1, "big"))
+                command = 0
+                command += 1 << 7  # ready bit
+                command += index << 5  # channel
+                command += 1 << 4  # mode
+                command += 0b10 << 2  # rate
+                command += 0  # gain
+                self.i2c.writeto(104, command.to_bytes(1, "big"))
                 await sleep_ms(250)
                 data = self.i2c.readfrom(104, 3)
                 raw = int.from_bytes(data[:2], "big")
@@ -145,9 +148,12 @@ class VoltMeter(HomieNode):
                 voltage = raw * 0.000_062_5
                 voltage *= 10
                 print(voltage)
-                self.display.show(voltage)
-                await sleep_ms(250)
                 self.measured_voltages[index] = voltage
+                await sleep_ms(0)
+            # update display
+            self.display.show(self.measured_voltages[self.currently_displayed_channel])
+            self.display.show_channel(self.currently_displayed_channel + 1)
+            await sleep_ms(50)
 
     def get_uptime(self):
         diff = int(time.time() - self.start)
@@ -163,6 +169,19 @@ class VoltMeter(HomieNode):
         # seconds
         out += str(diff) + "S"
         return out
+
+    async def advance_channel_debounce(self):
+        print("on button press")
+        irq_state = machine.disable_irq()
+        self.currently_displayed_channel += 1
+        if self.currently_displayed_channel >= 4:
+            self.currently_displayed_channel = 0
+        await sleep_ms(100)
+        machine.enable_irq(irq_state)
+
+    def on_button_press(self, *args, **kwargs):
+        loop = get_event_loop()
+        loop.create_task(self.advance_channel_debounce())
 
 def main():
     print("homie main")
--- firmware/max9651.py
+++ firmware/max9651.py
@@ -8,25 +8,29 @@ class MAX9651(object):
     def __init__(self):
         self.spi = SPI(sck=Pin(14), mosi=Pin(13), miso=Pin(15), baudrate=50_000, polarity=0)
         self.cs = Pin(12, mode=Pin.OUT, value=1)
-        self._write_bytes(b"\x01\x3E")  # decode mode
+        self._write_bytes(b"\x01\x1F")  # decode mode
         self._write_bytes(b"\x02\x7F")  # intensity
-        self._write_bytes(b"\x03\x05")  # display six digits
+        self._write_bytes(b"\x03\x06")  # display six digits
         self._write_bytes(b"\x04\x21")  # clear
+        self._sign_pin = Pin(0, Pin.OUT)
 
     def show(self, num: float):
         # sign
         if num >= 0.0:
-            self._write_bytes(b"\x60\x00")
+            self._sign_pin.off()
         else:
-            self._write_bytes(b"\x60\x01")
+            self._sign_pin.on()
         # value
         string = "{:.3f}".format(abs(num))
         string = ("0" * (6 - len(string))) + string
-        self._write_bytes(b"\x61" + int(string[0]).to_bytes(1, "big"))
-        self._write_bytes(b"\x62" + (int(string[1]) | 128).to_bytes(1, "big"))
-        self._write_bytes(b"\x63" + int(string[3]).to_bytes(1, "big"))
-        self._write_bytes(b"\x64" + int(string[4]).to_bytes(1, "big"))
-        self._write_bytes(b"\x65" + int(string[5]).to_bytes(1, "big"))
+        print(string)
+        self._write_bytes(b"\x60" + int(string[0]).to_bytes(1, "big"))
+        self._write_bytes(b"\x61" + (int(string[1]) | 128).to_bytes(1, "big"))
+        self._write_bytes(b"\x62" + int(string[3]).to_bytes(1, "big"))
+        self._write_bytes(b"\x63" + int(string[4]).to_bytes(1, "big"))
+
+    def show_channel(self, num):
+        self._write_bytes(b"\x64" + num.to_bytes(1, "big"))
 
     def _write_bytes(self, data: bytes):
         time.sleep(0.01)
